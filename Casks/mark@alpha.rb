4e6dd215953fe14b6fcefa76950a1f2e2d3f1884
xronocode <myevdokimov@gmail.com>
2026-05-21 11:49:14 +0600

    chore: update alpha.7 SHA (drag fix)

--- Casks/mark@alpha.rb
+++ Casks/mark@alpha.rb
@@ -15,7 +15,7 @@ cask "mark@alpha" do
   # beta. Stay on `mark` (v1.2.x) for those.
 
   version "2.0.0-alpha.7"
-  sha256 "4f30efce707bb8513663e2e4fa1ecc2b98a547757faa7fb06830dc46aebd2342"
+  sha256 "528bba4fab018202486c4584a874741abacec3de8da8427c7cb26abdcb3ff28b"
 
   on_arm do
     url "https://github.com/xronocode/mark/releases/download/v#{version}/Mark_#{version}_aarch64.dmg",
