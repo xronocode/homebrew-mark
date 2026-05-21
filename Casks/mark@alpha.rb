a024b3feedd32284b813e56a5c3ee0b0279a69e3
xronocode <myevdokimov@gmail.com>
2026-05-21 11:25:36 +0600

    chore: update alpha.7 SHA (dark-theme + titlebar fix)

--- Casks/mark@alpha.rb
+++ Casks/mark@alpha.rb
@@ -15,7 +15,7 @@ cask "mark@alpha" do
   # beta. Stay on `mark` (v1.2.x) for those.
 
   version "2.0.0-alpha.7"
-  sha256 "3dbca2c41e88d75f08398470a07c8acffc9f215c7ffb3779973eedfdcc377da9"
+  sha256 "3049cbd0b215467bf9db4fdffc4523edd5fb3e59d356b90b66bbd9d763f58504"
 
   on_arm do
     url "https://github.com/xronocode/mark/releases/download/v#{version}/Mark_#{version}_aarch64.dmg",
